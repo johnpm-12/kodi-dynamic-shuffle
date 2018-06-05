78b0e0f587bd5063089823daf2a8638fb632ef4f
whatsyourgithub <39016062+whatsyourgithub@users.noreply.github.com>
2018-06-04 23:19:42 -0700

    changes

--- addon.py
+++ addon.py
@@ -26,60 +26,58 @@ import xbmcgui
 class myPlayer(xbmc.Player):
     def __init__(self):
         xbmc.Player.__init__(self)
+        self.cwd = xbmcaddon.Addon().getAddonInfo('path').decode('utf-8')
+        with open(self.cwd + '\\playlistdir.txt') as playlistDirFile:
+            playlistDirPath = playlistDirFile.readline().decode('utf-8').strip()
+        with open(self.cwd + '\\playlist.json') as playlistFile:
+            self.playlistDict = json.load(playlistFile)
+        playlistObj = []
+        for root, _, files in os.walk(playlistDirPath):
+            for filename in files:
+                filepath = root + '\\' + filename
+                if filepath in self.playlistDict:
+                    playlistObj.append((self.playlistDict[filepath], filepath))
+                else:
+                    playlistObj.append((0, filepath))
+
+        random.shuffle(playlistObj)
+        heapq.heapify(playlistObj)
+        lastTime = 0
+        lastIndex = 0
+        for index in xrange(len(playlistObj)):
+            if playlistObj[index][0] != lastTime:
+                startList = playlistObj[:lastIndex]
+                shuffleList = playlistObj[lastIndex:index]
+                endList = playlistObj[index:]
+                random.shuffle(shuffleList)
+                playlistObj = startList + shuffleList + endList
+                lastTime = playlistObj[index][0]
+                lastIndex = index
+        startList = playlistObj[:lastIndex]
+        shuffleList = playlistObj[lastIndex:]
+        random.shuffle(shuffleList)
+        playlistObj = startList + shuffleList
+        playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
+        playlist.clear()
+        for item in playlistObj:
+            fname = item[1].rsplit('\\', 1)[1]
+            listitem = xbmcgui.ListItem(fname)
+            listitem.setInfo('video', {'Title': fname})
+            playlist.add(item[1], listitem=listitem)
+        self.play(playlist)
+
+    def onPlayBackStarted(self):
+        self.currentFilePath = self.getPlayingFile()
 
     def onPlayBackEnded(self):
-        global playlistDict
-        global playlistObj
-        global playlistIndex
-        global cwd
-        playlistDict[playlistObj[playlistIndex][1]] = time.time()
-        with open(cwd + '\\playlist.json', 'w') as playlistFile:
-            json.dump(playlistDict, playlistFile)
-        playlistIndex += 1
+        self.playlistDict[self.currentFilePath] = time.time()
+        with open(self.cwd + '\\playlist.json', 'w') as playlistFile:
+            json.dump(self.playlistDict, playlistFile)
 
 
 busyDialog = xbmcgui.DialogBusy()
 busyDialog.create()
-cwd = xbmcaddon.Addon().getAddonInfo('path').decode('utf-8')
-with open(cwd + '\\playlistdir.txt') as playlistDirFile:
-    playlistDirPath = playlistDirFile.readline().decode('utf-8').strip()
-with open(cwd + '\\playlist.json') as playlistFile:
-    playlistDict = json.load(playlistFile)
-playlistObj = []
-for root, dirs, files in os.walk(playlistDirPath):
-    for filename in files:
-        filepath = root + '\\' + filename
-        if filepath in playlistDict:
-            playlistObj.append((playlistDict[filepath], filepath))
-        else:
-            playlistObj.append((0, filepath))
-playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
-random.shuffle(playlistObj)
-heapq.heapify(playlistObj)
-lastTime = 0
-lastIndex = 0
-for index in xrange(len(playlistObj)):
-    if playlistObj[index][0] != lastTime:
-        startList = playlistObj[:lastIndex]
-        shuffleList = playlistObj[lastIndex:index]
-        endList = playlistObj[index:]
-        random.shuffle(shuffleList)
-        playlistObj = startList + shuffleList + endList
-        lastTime = playlistObj[index][0]
-        lastIndex = index
-startList = playlistObj[:lastIndex]
-shuffleList = playlistObj[lastIndex:]
-random.shuffle(shuffleList)
-playlistObj = startList + shuffleList
-for item in playlistObj:
-    fname = item[1].rsplit('\\', 1)[1]
-    listitem = xbmcgui.ListItem(fname)
-    listitem.setInfo('video', {'Title': fname})
-    playlist.add(item[1], listitem=listitem)
 player = myPlayer()
-playlistIndex = 0
-player.play(playlist)
 busyDialog.close()
-xbmc.sleep(500)
-while player.isPlaying():
+while True:
     xbmc.sleep(500)
